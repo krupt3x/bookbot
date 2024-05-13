b91153d36c76c9b86074aa2a76e6d266b93bbfe9
krupt3x <161846850+krupt3x@users.noreply.github.com>
2024-05-13 21:13:11 +0530

    add the functionality to count words

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 console.log("Bookbot has been initialized.");
+console.log("Hold up bookbot's counting words.")
+const fs = require('node:fs');
+
+fs.readFile('./books/frankenstein.txt', 'utf8', (err, data) => {
+    if (err) {
+        throw err;
+    }
+
+    const words = data.split(/\s+/);
+    const numWords = words.length;
+
+    console.log(`The file contains ${numWords} words.`);
+});
+
+console.log("bookbot exiting gracefully.")
