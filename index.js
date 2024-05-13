5b69f71b803574c921d0cf09085eba1f9edeab5e
krupt3x <161846850+krupt3x@users.noreply.github.com>
2024-05-13 21:17:33 +0530

    wrap the logic in main function

--- index.js
+++ index.js
@@ -1,16 +1,19 @@
 console.log("Bookbot has been initialized.");
 console.log("Hold up bookbot's counting words.")
+
 const fs = require('node:fs');
 
-fs.readFile('./books/frankenstein.txt', 'utf8', (err, data) => {
-    if (err) {
-        throw err;
-    }
+function main() {
+    fs.readFile('./books/frankenstein.txt', 'utf8', (err, data) => {
+        if (err) {
+            throw err;
+        }
 
-    const words = data.split(/\s+/);
-    const numWords = words.length;
+        const words = data.split(/\s+/);
+        const numWords = words.length;
 
-    console.log(`The file contains ${numWords} words.`);
-});
-
+        console.log(`The file contains ${numWords} words.`);
+    });
+}
+main()
 console.log("bookbot exiting gracefully.")
